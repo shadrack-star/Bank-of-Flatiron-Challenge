c6ff4047472f047d5116260d68c18a62a16e884b
Shadrack <58778974+shadrack-star@users.noreply.github.com>
2024-05-02 17:43:22 +0300

    Updated

--- backend/src/components/TransactionsList.js
+++ backend/src/components/TransactionsList.js
@@ -8,7 +8,7 @@ function TransactionsList()
   
     useEffect(()=>{
   
-      fetch('http://localhost:8001/transactions')
+      fetch('https://bank-of-flatiron-challenge-4.onrender.com/transactions')
         .then((response) => response.json())
         .then((data) => {
   
